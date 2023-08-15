7bd69299cecafdc81e20394a5b482e8fd28a5ec5
Marty Zalega <marty@zalega.me>
2023-08-15 17:13:56 +1000

    Update to version 0.1.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Oban.LiveDashboard.MixProject do
   def project do
     [
       app: :oban_live_dashboard,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),

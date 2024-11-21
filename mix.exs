d56db7da646c179c4ff545b2327dc51621a0bd7b
Marty Zalega <marty@zalega.me>
2024-11-22 07:16:10 +1000

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Oban.LiveDashboard.MixProject do
   def project do
     [
       app: :oban_live_dashboard,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
